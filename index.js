28bb81696c27256dd5f2bccdb8824407ea263e1f
Aldorith <connorjohnbacon@gmail.com>
2022-03-08 23:40:01 -0800

    -Added addCommunity() to be used by communityCreator in the client

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function main() {
   app.post('/getUserData', jsonParser, async function (req, res) {
     console.log("\nAPI REQUEST RECEIVED");
 
-    // Establish Database Conneciton
+    // Establish Database Connection
     const connection = establishConnection();
     const db = makeDb();
     await db.connect(connection);
@@ -52,7 +52,7 @@ async function main() {
         let sql = `INSERT into MEMBER VALUES ('${req.body.uid}', '${req.body.username}', '${req.body.firstName}', '${req.body.lastName}', '${req.body.email}', null)`;
         db.query(connection, sql);
 
-        // Now Requery the Database (This might be unnecessary as I think about it, but it keeps everything uniform at least, we may remove later)
+        // Now Re-query the Database (This might be unnecessary as I think about it, but it keeps everything uniform at least, we may remove later)
         sql = `SELECT * from MEMBER WHERE UserID = '${req.body.uid}'`;
         userData = await db.query(connection, sql);
 
@@ -76,6 +76,52 @@ async function main() {
     console.log(`Server listening on port ${port}`)
   })
 
+}
+
+async function addCommunity(){
+  app.post('/addCommunity', jsonParser, async function (req, res) {
+    console.log("\nAPI REQUEST RECEIVED");
+
+    // Establish Database Connection
+    const connection = establishConnection();
+    const db = makeDb();
+    await db.connect(connection);
+
+    // Setup Response Data
+    let communityData;
+
+    // Make Query
+    try {
+      let sql = `SELECT * from MEMBER WHERE communityID = '${req.body.communityID}'`;
+      communityData = await db.query(connection, sql);
+
+      if (communityData.length > 0) {
+        console.log("Error, that Community Already Exists");
+
+        console.log("Sending Data Back\n");
+        res.send(null);
+      }
+      else
+      {
+        console.log("Adding Community to Database");
+        let sql = `INSERT into MEMBER VALUES ('${req.body.communityID}', '${req.body.communityDesciption}',null)`;
+        db.query(connection, sql);
+
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await db.close(connection);
+    }
+
+    // Send the data back
+    console.log("Sending Data Back\n");
+    res.send(communityData);
+  })
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
 }
 
 main();
